2d036ac5c6ad15862232391f2ec72f24ba0496c4
emirot <emirot.nolan@gmail.com>
2022-03-04 22:15:23 -0800

    feat: add tests

--- src/sqlgojson/sqlgojson_test.go
+++ src/sqlgojson/sqlgojson_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func queryPostgresql() (*sql.Rows, error) {
+func queryPostgresql(query string) (*sql.Rows, error) {
 	connStr := "user=root dbname=sqljson password=test host=localhost sslmode=disable port=5432"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
@@ -18,7 +18,7 @@ func queryPostgresql() (*sql.Rows, error) {
 		panic(err)
 	}
 	fmt.Printf("\nSuccessfully connected to database \n")
-	rows, err := db.Query("SELECT * from users;")
+	rows, err := db.Query(query)
 	if err != nil {
 		panic(err)
 	}
@@ -26,7 +26,7 @@ func queryPostgresql() (*sql.Rows, error) {
 }
 
 func TestUserTable(t *testing.T) {
-	rows, err := queryPostgresql()
+	rows, err := queryPostgresql("SELECT * from users;")
 	if err != nil {
 		panic(err)
 	}
@@ -35,6 +35,23 @@ func TestUserTable(t *testing.T) {
 		panic(err)
 	}
 	if res != `[[{"id":1},{"name":"nolan"},{"created_at":"2017-07-01T14:59:55.711Z"}],[{"id":2},{"name":"test"},{"created_at":"2017-07-01T15:59:55.711Z"}],[{"id":3},{"name":"test"},{"created_at":null}]]` {
+		t.Errorf("Failed")
+	}
+	fmt.Printf("rep:%v", res)
+}
+
+func TestTestTable(t *testing.T) {
+	rows, err := queryPostgresql("SELECT * from test;")
+	if err != nil {
+		panic(err)
+	}
+	res, err := SqlGoJson(rows)
+	if err != nil {
+		panic(err)
+	}
+	expected := `[[{"id":1},{"num":3.14},{"b":true}],[{"id":2},{"num":4.014},{"b":false}]]`
+	fmt.Println(res, expected)
+	if res != expected {
 		t.Errorf("Failed")
 	}
 	fmt.Printf("rep:%v", res)
