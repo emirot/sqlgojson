ccf3bddfd275f7148dadd203abe74e1864eca57f
emirot <emirot.nolan@gmail.com>
2022-03-04 22:15:23 -0800

    feat: add tests

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -7,3 +7,12 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users values (1, 'nolan', '2017-07-01T14:59:55.711Z');
 INSERT INTO users values (2, 'test', '2017-07-01T15:59:55.711Z');
 INSERT INTO users values (3, 'test', null);
+
+CREATE TABLE IF NOT EXISTS test (
+    id INT,
+    num FLOAT DEFAULT NULL,
+    b boolean DEFAULT NULL
+);
+
+INSERT INTO test values (1, 3.14, TRUE);
+INSERT INTO test values (2, 4.014, FALSE);
